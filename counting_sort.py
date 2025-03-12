d751b6b683cb263e6ce4b3cd88ce03c74772255d
Yu Heng Seow <seowyh98@gmail.com>
2025-03-12 13:39:51 +1100

    implemented stable counting sort.

--- counting_sort.py
+++ counting_sort.py
@@ -12,5 +12,23 @@ def counting_sort(arr):
             arr[counter] = i
             counter += 1
     return arr
-        
-print(counting_sort([1,2,3,4,5,5,3,7]))
+
+
+def stable_counting_sort(arr):
+    max_num = arr[0]
+    for i in range(1, len(arr)):
+        if max_num < arr[i]:
+            max_num = arr[i]
+    count_arr = [None] * (max_num + 1)
+    for i in range(max_num + 1):
+        count_arr[i] = []
+    for i in range(len(arr)):
+        count_arr[arr[i]].append(arr[i])
+    counter = 0
+    for i in range(len(count_arr)):
+        for j in range(len(count_arr[i])):
+            arr[counter] = count_arr[i][j]
+            counter += 1
+    return arr
+print(stable_counting_sort([1,2,3,4,5,5,3,7]))
+

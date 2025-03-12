8ce171c460be8ac6338d9211921bb8b9171a19db
Yu Heng Seow <seowyh98@gmail.com>
2025-03-12 23:21:04 +1100

    Implemented radix sort with desired partition

--- radix_sort.py
+++ radix_sort.py
@@ -7,20 +7,23 @@ def radix_sort(arr, no_of_digit):
             max_num = arr[i]
     col = int(log(max_num, 10)) + 1
     arr = [(i, None) for i in arr]
-    for i in range(col//no_of_digit):
+    for i in range(int(col//no_of_digit)+1):
         # cur = 10 ** i
         count_arr = [None] * (10 ** no_of_digit)
         # initiliazing empty seperate chaining for each iteration
         for j in range((10 ** no_of_digit)):
             count_arr[j] = []
         for j in range(len(arr)):
-            count_arr[(arr[j][0]%10**(i+1))//10**i].append(arr[j])
+            count_arr[(arr[j][0]%10**((i+1)*no_of_digit))//10**(i*no_of_digit)].append(arr[j])
         counter = 0
         for i in range(len(count_arr)):
             for j in range(len(count_arr[i])):
                 arr[counter] = count_arr[i][j]
                 counter += 1
+            if counter == len(arr):
+                break
     return arr
 
-# print(radix_sort([1234098,8970987,2346643,10012310],3))
-print(radix_sort([464,392,554,402],1))
+print(radix_sort([1234098,8970987,2346643,10012310],3))
+# print(sorted([1234098,8970987,2346643,10012310]))
+# print(radix_sort([464,392,554,402],1))
